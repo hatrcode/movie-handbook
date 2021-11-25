36d22df49ea43fcddd6c1e73950890f76b1dd482
Ha Truong <td.xuha@gmail.com>
2021-11-25 14:51:31 +0700

    config(gatsby-config.js): update url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://movie-explore.netlify.app/",
     title: "Movie Handbook",
   },
   plugins: [
