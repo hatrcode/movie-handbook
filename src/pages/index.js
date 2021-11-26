584407c307856c5461f7c782e2fd7fa22eb33405
Ha Truong <td.xuha@gmail.com>
2021-11-26 21:42:05 +0700

    fix(index.js): remove title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Layout from "../components/layout/Layout";
 const IndexPage = () => {
   return (
     <Layout>
-      <h1 className="title">Movie Handbook</h1>
       <MovieSearch />
     </Layout>
   );
