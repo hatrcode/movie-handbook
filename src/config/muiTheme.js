b296bb7af9f1321cf7c02be1ec93dc3a2ca80f85
Ha Truong <td.xuha@gmail.com>
2021-12-01 16:49:04 +0700

    feat: config mui themes

---
+++ src/config/muiTheme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+import { blue, pink } from "@mui/material/colors";
+
+const muiTheme = createTheme({
+  root: {
+    "& > *": {
+      justifyContent: "center",
+      display: "flex",
+    },
+  },
+  palette: {
+    primary: blue,
+    secondary: pink,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+  },
+});
+
+export default muiTheme;
