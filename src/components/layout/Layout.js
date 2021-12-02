f320a07a7975a25b79f053168f6033f6546e331a
Ha Truong <td.xuha@gmail.com>
2021-12-02 22:22:41 +0700

    style(Layout.js): add SubNav

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,15 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../../css/main.css";
-
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import BottomNav from "./BottomNav";
+import SubNav from "./SubNav";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <SubNav />
       {children}
       <BottomNav />
       <Footer />
