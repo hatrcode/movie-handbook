da305037df38ad2f1d7bce8c69615b3cca95de56
Ha Truong <td.xuha@gmail.com>
2021-12-02 22:28:54 +0700

    cont: update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,23 @@
-import * as React from "react";
+import React from "react";
+import Layout from "../components/layout/Layout";
 import { Link } from "gatsby";
+import Seo from "../components/layout/SEO";
 
-const NotFoundPage = () => {
+const Error = () => {
   return (
-    <div>
-      <h1>Page not found</h1>
-      <Link to="/">Go home</Link>.
-    </div>
+    <Layout>
+      <Seo title="Page Not Found" />
+      <main className="error-page">
+        <div className="error-container">
+          <h1>404 Page</h1>
+          <p>Sorry. We couldn’t find what you were looking for.</p>
+          <Link to="/" className="btn">
+            Back to Homepage
+          </Link>
+        </div>
+      </main>
+    </Layout>
   );
 };
 
-export default NotFoundPage;
+export default Error;
