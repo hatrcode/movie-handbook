ea095fc6efb609779a87c3be264c4f64809f2f8c
Ha Truong <td.xuha@gmail.com>
2021-12-02 22:20:07 +0700

    feat: add SEO component

---
+++ src/components/layout/SEO.js
@@ -0,0 +1,214 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteUrl
+        defaultTitle: title
+        defaultDescription: description
+        defaultBanner: banner
+        headline
+        siteLanguage
+        ogLanguage
+        author
+        twitter
+        facebook
+      }
+      buildTime(formatString: "MMMM D, YYYY")
+    }
+  }
+`
+
+const SEO = ({ title, desc, banner, pathname, article, datePub, dateMod }) => {
+  const { site } = useStaticQuery(query)
+
+  const {
+    buildTime,
+    siteMetadata: {
+      siteUrl,
+      defaultTitle,
+      defaultDescription,
+      defaultBanner,
+      headline,
+      siteLanguage,
+      ogLanguage,
+      author,
+      twitter,
+      facebook,
+    },
+  } = site
+
+  const seo = {
+    title: title || defaultTitle,
+    description: desc || defaultDescription,
+    image: banner ? `${siteUrl}${banner}` : `${siteUrl}/${defaultBanner}`,
+    url: `${siteUrl}${pathname || ""}`,
+  }
+
+  const schemaOrgWebPage = {
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    url: siteUrl,
+    headline,
+    inLanguage: siteLanguage,
+    mainEntityOfPage: siteUrl,
+    description: defaultDescription,
+    name: defaultTitle,
+    author: {
+      "@type": "Person",
+      name: author,
+    },
+    copyrightHolder: {
+      "@type": "Person",
+      name: author,
+    },
+    copyrightYear: "2021",
+    creator: {
+      "@type": "Person",
+      name: author,
+    },
+    publisher: {
+      "@type": "Person",
+      name: author,
+    },
+    datePublished: "2021-01-01",
+    dateModified: buildTime,
+    image: {
+      "@type": "ImageObject",
+      url: `${siteUrl}/${defaultBanner}`,
+    },
+  }
+
+  // Initial breadcrumb list
+
+  const itemListElement = [
+    {
+      "@type": "ListItem",
+      item: {
+        "@id": siteUrl,
+        name: "Homepage",
+      },
+      position: 1,
+    },
+  ]
+
+  let schemaArticle = null
+
+  if (article) {
+    schemaArticle = {
+      "@context": "http://schema.org",
+      "@type": "Article",
+      author: {
+        "@type": "Person",
+        name: author,
+      },
+      copyrightHolder: {
+        "@type": "Person",
+        name: author,
+      },
+      copyrightYear: "2021",
+      creator: {
+        "@type": "Person",
+        name: author,
+      },
+      publisher: {
+        "@type": "Organization",
+        name: author,
+        logo: {
+          "@type": "ImageObject",
+          url: `${siteUrl}/${defaultBanner}`,
+        },
+      },
+      datePublished: datePub,
+      dateModified: dateMod,
+      description: seo.description,
+      headline: seo.title,
+      inLanguage: siteLanguage,
+      url: seo.url,
+      name: seo.title,
+      image: {
+        "@type": "ImageObject",
+        url: seo.image,
+      },
+      mainEntityOfPage: seo.url,
+    }
+    // Push current blogpost into breadcrumb list
+    itemListElement.push({
+      "@type": "ListItem",
+      item: {
+        "@id": seo.url,
+        name: seo.title,
+      },
+      position: 2,
+    })
+  }
+
+  const breadcrumb = {
+    "@context": "http://schema.org",
+    "@type": "BreadcrumbList",
+    description: "Breadcrumbs list",
+    name: "Breadcrumbs",
+    itemListElement,
+  }
+
+  return (
+    <>
+      <Helmet title={`${seo.title} | ${defaultTitle}`}>
+        <html lang={siteLanguage} />
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={seo.image} />
+        {/* Insert schema.org data conditionally (webpage/article) + everytime (breadcrumbs) */}
+        {!article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaOrgWebPage)}
+          </script>
+        )}
+        {article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaArticle)}
+          </script>
+        )}
+        <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script>
+        {facebook && <meta property="og:site_name" content={facebook} />}
+        <meta property="og:locale" content={ogLanguage} />
+        <meta property="og:url" content={seo.url} />
+        <meta property="og:type" content={article ? "article" : "website"} />
+        <meta property="og:title" content={`${seo.title} | ${defaultTitle}`} />
+        <meta property="og:description" content={seo.description} />
+        <meta property="og:image" content={seo.image} />
+        <meta property="og:image:alt" content={seo.description} />
+        <meta name="twitter:creator" content={twitter} />
+        <meta name="twitter:site" content={twitter} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={seo.title} />
+        <meta name="twitter:description" content={seo.desc} />
+        <meta name="twitter:image" content={seo.image} />
+        <meta name="twitter:image:alt" content={seo.desc} />
+      </Helmet>
+    </>
+  )
+}
+
+export default SEO
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  desc: PropTypes.string,
+  banner: PropTypes.string,
+  pathname: PropTypes.string,
+  article: PropTypes.bool,
+  node: PropTypes.object,
+}
+
+SEO.defaultProps = {
+  title: null,
+  desc: null,
+  banner: null,
+  pathname: null,
+  article: false,
+  node: null,
+}
