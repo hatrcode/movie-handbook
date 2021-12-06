9078cd37d1a953a5ab2d66ec291e25ba945b9e88
Ha Truong <td.xuha@gmail.com>
2021-12-06 16:03:49 +0700

    fix(Layout): remove SubNav

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,13 +4,11 @@ import "../../css/main.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import BottomNav from "./BottomNav";
-import SubNav from "./SubNav";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <SubNav />
       {children}
       <BottomNav />
       <Footer />
