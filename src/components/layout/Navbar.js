ffbae996ecba192d3ebf8782b7ca7ef4a3cd77f9
Ha Truong <td.xuha@gmail.com>
2021-12-06 16:06:51 +0700

    feat(Navbar): add mobile menu

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,51 @@
 import React from "react";
 import { Link } from "gatsby";
-import { social } from "../../constants/navbarLinks";
 import SearchBar from "../SearchBar";
+import SubNavDrop from "../layout/SubNavDrop";
+import { movies, series, trending } from "../../constants/navbarLinks";
+import { Drawer, Box, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={() => setMobileOpen(!mobileOpen)}
+            sx={{ mr: 2, display: { md: "none" } }}>
+            <MenuIcon />
+          </IconButton>
           <div className="logo">
             <Link to="/">Movie Handbook</Link>
           </div>
+          <Box component="div" sx={{ display: { md: "none" } }}>
+            <Link to="/search">
+              <SearchIcon />
+            </Link>
+          </Box>
         </div>
-        <SearchBar />
-        <ul className="social-icons">
-          {social.map((socialIcon) => {
-            const { id, url, icon } = socialIcon;
-            return (
-              <li key={id}>
-                <a href={url}>{icon}</a>
-              </li>
-            );
-          })}
-        </ul>
+        <Box component="div" sx={{ display: { xs: "none", md: "block" } }}>
+          <SubNavDrop main="Movies" url="/movies" sub={movies} />
+          <SubNavDrop main="Series" url="/series" sub={series} />
+          <SubNavDrop main="Trending" url="/trending" sub={trending} />
+        </Box>
+        <Box component="div" sx={{ display: { xs: "none", md: "block" } }}>
+          <SearchBar />
+        </Box>
+
+        <Box component="div" sx={{ display: { xs: "block" } }}>
+          <Drawer open={mobileOpen} onClose={() => setMobileOpen(!mobileOpen)}>
+            <MobileMenu />
+          </Drawer>
+        </Box>
       </div>
     </nav>
   );
