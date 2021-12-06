4e5e4d47259b8f449aa1d336b0644bd31e6ed39b
Ha Truong <td.xuha@gmail.com>
2021-12-06 15:10:57 +0700

    feat(trending.js): update url!

--- src/pages/trending.js
+++ src/pages/trending.js
@@ -4,10 +4,13 @@ import MovieCard from "../components/MovieCard";
 import Layout from "../components/layout/Layout";
 import ItemPagination from "../components/ItemPagination";
 
-const Trending = () => {
+const Trending = ({ location }) => {
   const [page, setPage] = useState(1);
+  const params = new URLSearchParams(location.search);
+  const media_type = params.get("media_type") || "all";
+  const time = params.get("time") || "week";
 
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
+  const url = `https://api.themoviedb.org/3/trending/${media_type}/${time}?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
 
   const { content, numOfPages } = useFetch(url, page);
 
