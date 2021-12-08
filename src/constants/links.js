580fc5de3b37d691d70cfcfeedd4c24227b43485
Ha Truong <td.xuha@gmail.com>
2021-12-08 18:53:05 +0700

    cont: update links

---
+++ src/constants/links.js
@@ -0,0 +1,7 @@
+export const img_300 = "https://image.tmdb.org/t/p/w300";
+export const img_500 = "https://image.tmdb.org/t/p/w500";
+export const img_1920 = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces";
+export const unavailable =
+  "https://www.movienewz.com/img/films/poster-holder.jpg";
+
+export const noPic = "src/images/noPic.svg";
