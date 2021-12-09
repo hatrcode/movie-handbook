063658f70fed5793e11b770a48cf7d140e9dbc25
Ha Truong <td.xuha@gmail.com>
2021-12-09 22:27:58 +0700

    styl(MovieCard.js): update card component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { img_300, unavailable } from "../constants/links";
-import ImageListItem from "@mui/material/ImageListItem";
-import ImageListItemBar from "@mui/material/ImageListItemBar";
-import IconButton from "@mui/material/IconButton";
-import InfoIcon from "@mui/icons-material/Info";
 import Popover from "@mui/material/Popover";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
@@ -40,68 +36,66 @@ const MovieCard = ({ movie }) => {
   const cardTitle = `${defaultTitle} (${year})`;
   const text = overview.split(".")[0];
   return (
-    <Box container sx={{ width: { xs: 150, sm: 185 } }}>
-      <ImageListItem
+    <Box container sx={{ minWidth: { xs: 100, sm: 150 } }}>
+      <Box
         onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}>
+        onMouseLeave={handlePopoverClose}
+        sx={{
+          position: "relative",
+          display: "inline-block",
+          lineHeight: 0,
+        }}>
         <img
           src={poster_path ? `${img_300}${poster_path}` : unavailable}
-          alt={title}
+          alt={defaultTitle}
           loading="lazy"
+          width="100%"
         />
-        <ImageListItemBar
-          subtitle={cardTitle}
-          sx={{ fontSize: "0.5rem" }}
-          actionIcon={
-            <IconButton
-              sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-              aria-label={`info about ${title || name} `}>
-              <InfoIcon />
-            </IconButton>
-          }
-        />
-        <div>
-          <Popover
-            id="mouse-over-popover"
-            sx={{
-              pointerEvents: "none",
-            }}
-            open={open}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: "center",
-              horizontal: "center",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            onClose={handlePopoverClose}
-            disableRestoreFocus>
-            <Card sx={{ maxWidth: 300, pointerEvents: "auto" }}>
-              <CardContent>
-                <Typography variant="h6" component="h3" gutterBottom>
-                  {cardTitle}
-                  <Button
-                    size="small"
-                    variant="contained"
-                    disabled
-                    sx={{ ml: 1 }}>
-                    TMDb: {vote_average}
-                  </Button>
-                </Typography>
-                <Typography variant="body2">{text}.</Typography>
-                {/* <Button
+        <div className="movie-info">
+          <p style={{ marginBottom: "0" }}>{defaultTitle}</p>
+        </div>
+      </Box>
+      <div>
+        <Popover
+          id="mouse-over-popover"
+          sx={{
+            pointerEvents: "none",
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "center",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus>
+          <Card sx={{ maxWidth: 300, pointerEvents: "auto" }}>
+            <CardContent>
+              <Typography variant="h6" component="h3" gutterBottom>
+                {cardTitle}
+                <Button
+                  size="small"
+                  variant="contained"
+                  disabled
+                  sx={{ ml: 1 }}>
+                  TMDb: {vote_average}
+                </Button>
+              </Typography>
+              <Typography variant="body2">{text}.</Typography>
+              {/* <Button
                   size="small"
                   variant="contained"
                   sx={{ backgroundColor: "black", color: "white", mt: 1 }}>
                   Learn More
                 </Button> */}
-              </CardContent>
-            </Card>
-          </Popover>
-        </div>
-      </ImageListItem>
+            </CardContent>
+          </Card>
+        </Popover>
+      </div>
     </Box>
   );
 };
