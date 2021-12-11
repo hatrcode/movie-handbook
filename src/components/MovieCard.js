1946c0abf203a13c2d0aa5be722facb8a63eb804
Ha Truong <td.xuha@gmail.com>
2021-12-11 21:59:49 +0700

    feat(MovieCard): query info for people

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import { img_300, unavailable } from "../constants/links";
 import Popover from "@mui/material/Popover";
@@ -9,10 +9,23 @@ import Button from "@mui/material/Button";
 import { Box } from "@mui/system";
 
 const MovieCard = ({ movie }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  // Controller for Popover
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+
   const {
     id,
     title,
     name,
+    profile_path,
+    media_type,
     poster_path,
     overview,
     vote_average,
@@ -20,31 +33,22 @@ const MovieCard = ({ movie }) => {
     first_air_date,
   } = movie;
 
+  // Set type
   let type = "";
 
-  if (title) {
+  if (media_type === "person") {
+    type = "person";
+  } else if (title) {
     type = "movie";
   } else {
     type = "show";
   }
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const imgUrl = poster_path || profile_path;
+  const year = year
+    ? new Date(release_date || first_air_date).getFullYear()
+    : null;
 
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-
-  const year = new Date(release_date || first_air_date).getFullYear();
-
-  const defaultTitle = title || name;
-  const cardTitle = `${defaultTitle} (${year})`;
-  const text = overview.split(".")[0];
   return (
     <Box
       onMouseEnter={handlePopoverOpen}
@@ -55,56 +59,60 @@ const MovieCard = ({ movie }) => {
         lineHeight: 0,
       }}>
       <img
-        src={poster_path ? `${img_300}${poster_path}` : unavailable}
-        alt={defaultTitle}
+        src={imgUrl ? `${img_300}${imgUrl}` : unavailable}
+        alt={title || name}
         width="100%"
       />
       <div className="movie-info">
-        <p style={{ marginBottom: "0" }}>{defaultTitle}</p>
+        <p style={{ marginBottom: "0" }}>{title || name}</p>
       </div>
-      <div>
-        <Popover
-          id="mouse-over-popover"
-          sx={{
-            pointerEvents: "none",
-          }}
-          open={open}
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: "center",
-            horizontal: "center",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
-          onClose={handlePopoverClose}
-          disableRestoreFocus>
-          <Card sx={{ maxWidth: 300, pointerEvents: "auto" }}>
-            <CardContent>
-              <Typography variant="h6" component="h3" gutterBottom>
-                {cardTitle}
-                <Button
-                  size="small"
-                  variant="contained"
-                  disabled
-                  sx={{ ml: 1 }}>
-                  TMDb: {vote_average}
-                </Button>
-              </Typography>
-              <Typography variant="body2">{text}.</Typography>
-              <Link to={`/${type}/${id}`} state={{ id: id }}>
-                <Button
-                  size="small"
-                  variant="contained"
-                  sx={{ backgroundColor: "black", color: "white", mt: 1 }}>
-                  Learn More
-                </Button>
-              </Link>
-            </CardContent>
-          </Card>
-        </Popover>
-      </div>
+      {media_type !== "person" && (
+        <div>
+          <Popover
+            id="mouse-over-popover"
+            sx={{
+              pointerEvents: "none",
+            }}
+            open={open}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "center",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            onClose={handlePopoverClose}
+            disableRestoreFocus>
+            <Card sx={{ maxWidth: 300, pointerEvents: "auto" }}>
+              <CardContent>
+                <Typography variant="h6" component="h3" gutterBottom>
+                  {`${title || name} (${year})`}
+                  <Button
+                    size="small"
+                    variant="contained"
+                    disabled
+                    sx={{ ml: 1 }}>
+                    TMDb: {vote_average}
+                  </Button>
+                </Typography>
+                <Typography variant="body2">
+                  {overview.split(".")[0]}.
+                </Typography>
+                <Link to={`/${type}/${id}`} state={{ id: id }}>
+                  <Button
+                    size="small"
+                    variant="contained"
+                    sx={{ backgroundColor: "black", color: "white", mt: 1 }}>
+                    Learn More
+                  </Button>
+                </Link>
+              </CardContent>
+            </Card>
+          </Popover>
+        </div>
+      )}
     </Box>
   );
 };
