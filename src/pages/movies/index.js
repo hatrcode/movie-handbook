171573bed1519d3cdd9f6aeda1e13b3d097cb68e
Ha Truong <td.xuha@gmail.com>
2021-12-11 19:58:19 +0700

    feat(pages/movies): create movie folder with upcoming and toprated movies

---
+++ src/pages/movies/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import Genres from "../../components/Genres";
+import MovieCard from "../../components/MovieCard";
+import useGenre from "../../hooks/useGerne";
+import Layout from "../../components/layout/Layout";
+import Seo from "../../components/layout/SEO";
+import ItemPagination from "../../components/ItemPagination";
+import { Typography, Box } from "@mui/material";
+
+const Movies = () => {
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [page, setPage] = useState(1);
+  const genreforURL = useGenre(selectedGenres);
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.GATSBY_TMDB_API}&include_adult=false&page=${page}&with_genres=${genreforURL}`;
+  const { content, numOfPages, loading } = useFetch(url, page, selectedGenres);
+
+  return (
+    <Layout>
+      <Seo title="Movies" />
+      <div className="main-page">
+        <Typography variant="h3" component="h2" gutterBottom align="center">
+          Popular Movies
+        </Typography>
+        <Genres
+          type="movie"
+          selectedGenres={selectedGenres}
+          setSelectedGenres={setSelectedGenres}
+          genres={genres}
+          setGenres={setGenres}
+          setPage={setPage}
+        />
+        {loading && (
+          <Typography variant="h4" gutterBottom align="center">
+            Loading...
+          </Typography>
+        )}
+        <div className="item-list">
+          {content &&
+            content.map((movie) => (
+              <Box
+                container
+                key={movie.id}
+                sx={{ maxWidth: { xs: 150, md: 175 } }}>
+                <MovieCard movie={movie} media_type="movie" />
+              </Box>
+            ))}
+        </div>
+        {numOfPages > 1 && (
+          <ItemPagination setPage={setPage} numOfPages={numOfPages} />
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default Movies;
