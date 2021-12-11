dfe6af2349daa9eb302198bc363bfbb2a6a4c163
Ha Truong <td.xuha@gmail.com>
2021-12-11 19:58:59 +0700

    feat(pages/series.js): create shows folder with airing-today and top-rated shows

---
+++ src/pages/shows/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import Genres from "../../components/Genres";
+import MovieCard from "../../components/MovieCard";
+import useGenre from "../../hooks/useGerne";
+import Layout from "../../components/layout/Layout";
+import Seo from "../../components/layout/SEO";
+import ItemPagination from "../../components/ItemPagination";
+import { Box, Typography } from "@mui/material";
+
+const Shows = () => {
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [page, setPage] = useState(1);
+  const genreforURL = useGenre(selectedGenres);
+
+  const url = `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.GATSBY_TMDB_API}&include_adult=false&page=${page}&with_genres=${genreforURL}`;
+  const { content, numOfPages, loading } = useFetch(url, page, selectedGenres);
+
+  return (
+    <Layout>
+      <Seo title="Popular TV Shows" />
+      <div className="main-page">
+        <Typography variant="h3" component="h2" gutterBottom align="center">
+          Popular TV Shows
+        </Typography>
+        <Genres
+          type="tv"
+          selectedGenres={selectedGenres}
+          setSelectedGenres={setSelectedGenres}
+          genres={genres}
+          setGenres={setGenres}
+          setPage={setPage}
+        />
+        {loading && (
+          <Typography variant="h4" gutterBottom align="center">
+            Loading...
+          </Typography>
+        )}
+        <div className="item-list">
+          {content &&
+            content.map((movie) => (
+              <Box
+                container
+                key={movie.id}
+                sx={{ maxWidth: { xs: 150, md: 175 } }}>
+                <MovieCard movie={movie} media_type="tv" />
+              </Box>
+            ))}
+        </div>
+        {numOfPages > 1 && (
+          <ItemPagination setPage={setPage} numOfPages={numOfPages} />
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default Shows;
--- src/pages/shows/top-rated.js
+++ src/pages/shows/top-rated.js
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
-import { useFetch } from "../hooks/useFetch";
-import Genres from "../components/Genres";
-import MovieCard from "../components/MovieCard";
-import useGenre from "../hooks/useGerne";
-import Layout from "../components/layout/Layout";
-import Seo from "../components/layout/SEO";
-import ItemPagination from "../components/ItemPagination";
-import { Typography } from "@mui/material";
+import { useFetch } from "../../hooks/useFetch";
+import Genres from "../../components/Genres";
+import MovieCard from "../../components/MovieCard";
+import useGenre from "../../hooks/useGerne";
+import Layout from "../../components/layout/Layout";
+import Seo from "../../components/layout/SEO";
+import ItemPagination from "../../components/ItemPagination";
+import { Box, Typography } from "@mui/material";
 
-const Series = ({ location }) => {
-  const params = new URLSearchParams(location.search);
-  const year = params.get("year");
+const TopShows = () => {
   const [genres, setGenres] = useState([]);
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [page, setPage] = useState(1);
   const genreforURL = useGenre(selectedGenres);
 
-  const url = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.GATSBY_TMDB_API}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}&primary_release_year=${year}`;
+  const url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.GATSBY_TMDB_API}&include_adult=false&page=${page}&with_genres=${genreforURL}`;
   const { content, numOfPages, loading } = useFetch(url, page, selectedGenres);
 
   return (
     <Layout>
-      <Seo title="Series" />
+      <Seo title="Top Rated TV Shows" />
       <div className="main-page">
         <Typography variant="h3" component="h2" gutterBottom align="center">
-          TV Series
+          Top Rated TV Shows
         </Typography>
         <Genres
           type="tv"
@@ -42,7 +40,12 @@ const Series = ({ location }) => {
         <div className="item-list">
           {content &&
             content.map((movie) => (
-              <MovieCard movie={movie} key={movie.id} media_type="tv" />
+              <Box
+                container
+                key={movie.id}
+                sx={{ maxWidth: { xs: 150, md: 175 } }}>
+                <MovieCard movie={movie} media_type="tv" />
+              </Box>
             ))}
         </div>
         {numOfPages > 1 && (
@@ -53,4 +56,4 @@ const Series = ({ location }) => {
   );
 };
 
-export default Series;
+export default TopShows;
