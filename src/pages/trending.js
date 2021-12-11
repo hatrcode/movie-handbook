7e39fe6f40c1c04964e0a08681a726daab3e1a7e
Ha Truong <td.xuha@gmail.com>
2021-12-11 22:37:44 +0700

    fix(trending): update content

--- src/pages/trending.js
+++ src/pages/trending.js
@@ -37,9 +37,9 @@ const Trending = ({ location }) => {
             Loading...
           </Typography>
         )}
-        <div className="item-list">
-          {content &&
-            content.map((movie) => (
+        {content && (
+          <div className="item-list">
+            {content.map((movie) => (
               <Box
                 container
                 key={movie.id}
@@ -47,7 +47,8 @@ const Trending = ({ location }) => {
                 <MovieCard movie={movie} />
               </Box>
             ))}
-        </div>
+          </div>
+        )}
         {numOfPages > 1 && (
           <ItemPagination setPage={setPage} numOfPages={numOfPages} />
         )}
