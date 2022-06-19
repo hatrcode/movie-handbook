327ae477160b7d639aa22f19fe622bcdf7082391
Ha Truong <td.xuha@gmail.com>
2022-06-19 17:17:42 +0700

    refactor: clean pages movies and shows

--- src/pages/shows/airing-today.js
+++ src/pages/shows/airing-today.js
@@ -1,48 +1,13 @@
-import React, { useState } from "react";
-import { useFetch } from "../../hooks/useFetch";
-import Layout from "../../components/layout/Layout";
-import Seo from "../../components/layout/SEO";
-import Genres from "../../components/items/Genres";
-import useGenre from "../../hooks/useGerne";
-import ItemCards from "../../components/items/ItemCards";
-import ItemPagination from "../../components/items/ItemPagination";
-import { Typography } from "@mui/material";
+import React from "react";
+import ItemTemplate from "../../components/items/ItemTemplate";
 
 const AirShows = () => {
-  const [genres, setGenres] = useState([]);
-  const [selectedGenres, setSelectedGenres] = useState([]);
-  const [page, setPage] = useState(1);
-  const genreforURL = useGenre(selectedGenres);
-
-  const url = `https://api.themoviedb.org/3/tv/airing_today?api_key=${process.env.GATSBY_TMDB_API}&include_adult=false&page=${page}&with_genres=${genreforURL}`;
-  const { content, numOfPages, loading } = useFetch(url, page, selectedGenres);
-
   return (
-    <Layout>
-      <Seo title="TV Shows Airing Today" />
-      <div className="main-page">
-        <Typography variant="h3" component="h2" gutterBottom align="center">
-          TV Shows Airing Today
-        </Typography>
-        <Genres
-          type="tv"
-          selectedGenres={selectedGenres}
-          setSelectedGenres={setSelectedGenres}
-          genres={genres}
-          setGenres={setGenres}
-          setPage={setPage}
-        />
-        {loading && (
-          <Typography variant="h4" gutterBottom align="center">
-            Loading...
-          </Typography>
-        )}
-        {content && <ItemCards content={content} />}
-        {numOfPages > 1 && (
-          <ItemPagination setPage={setPage} numOfPages={numOfPages} />
-        )}
-      </div>
-    </Layout>
+    <ItemTemplate
+      media_type="tv"
+      filter="airing_today"
+      title="TV Shows Airing Today"
+    />
   );
 };
 
--- src/pages/shows/index.js
+++ src/pages/shows/index.js
@@ -1,48 +1,9 @@
-import React, { useState } from "react";
-import { useFetch } from "../../hooks/useFetch";
-import Layout from "../../components/layout/Layout";
-import Seo from "../../components/layout/SEO";
-import Genres from "../../components/items/Genres";
-import useGenre from "../../hooks/useGerne";
-import ItemCards from "../../components/items/ItemCards";
-import ItemPagination from "../../components/items/ItemPagination";
-import { Typography } from "@mui/material";
+import React from "react";
+import ItemTemplate from "../../components/items/ItemTemplate";
 
 const Shows = () => {
-  const [genres, setGenres] = useState([]);
-  const [selectedGenres, setSelectedGenres] = useState([]);
-  const [page, setPage] = useState(1);
-  const genreforURL = useGenre(selectedGenres);
-
-  const url = `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.GATSBY_TMDB_API}&include_adult=false&page=${page}&with_genres=${genreforURL}`;
-  const { content, numOfPages, loading } = useFetch(url, page, selectedGenres);
-
   return (
-    <Layout>
-      <Seo title="Popular TV Shows" />
-      <div className="main-page">
-        <Typography variant="h3" component="h2" gutterBottom align="center">
-          Popular TV Shows
-        </Typography>
-        <Genres
-          type="tv"
-          selectedGenres={selectedGenres}
-          setSelectedGenres={setSelectedGenres}
-          genres={genres}
-          setGenres={setGenres}
-          setPage={setPage}
-        />
-        {loading && (
-          <Typography variant="h4" gutterBottom align="center">
-            Loading...
-          </Typography>
-        )}
-        {content && <ItemCards content={content} />}
-        {numOfPages > 1 && (
-          <ItemPagination setPage={setPage} numOfPages={numOfPages} />
-        )}
-      </div>
-    </Layout>
+    <ItemTemplate media_type="tv" filter="popular" title="Popular TV Shows" />
   );
 };
 
